a52c0c748e02cb7c9a6fd1a19d7da93423399b06
Andrew Kirillov <kirillov.edu@gmail.com>
2020-05-05 17:12:22 -0400

    implemented track(), warmup(), trade(), and run() functions for simulation server scripts, and updated usage of constants & market objects

--- StableCoinSimulator/flask_functions.py
+++ StableCoinSimulator/flask_functions.py
@@ -3,51 +3,64 @@ sys.path.append("/Users/andrew/projects/bab-stablecoin_sims_research/StableCoinS
 
 from random import randint
 from simulator import *
-from flask import Flask, jsonify
-from flask_socketio import SocketIO
 
-app = Flask(__name__)
-socketio = SocketIO(app)
-
-market = Market()
-basis = BasisProtocol(int(1e7), market)
-market.protocol = basis
-traderPool = createTraderPool(basis, market, trader_demographics)
-market.setTraderPool(traderPool)
-
-cur_time = 0
-market.usd_eth = 1 / get_eth_price(0)
-
-trackers = {'usd_eth': [], 'bond_q_len': [], 'demand_ratio': [], 'ask_volume': [],
-            'bid_volume': [], 'total_volume': [], 'circulation': []}
-
-def track():
+def track(trackers, market):
     trackers['usd_eth'].append(market.usd_eth)
-    trackers['bond_q_len'].append(basis.bondQueueLength)
+    trackers['bond_q_len'].append(market.protocol.bondQueueLength)
     trackers['demand_ratio'].append(market.demandRatio)
     trackers['ask_volume'].append(market.askVolume)
     trackers['bid_volume'].append(market.bidVolume)
     trackers['total_volume'].append(market.totalVolume)
     trackers['circulation'].append(market.getCirculation())
 
-@app.route('/warmup')
-def warmup():
+def warmup(market):
     global cur_time
-    NUM_TRADERS = trader_demographics['IdealTrader'] # We initially set it up with randomTraders
+    NUM_TRADERS = market.params["trader_demographics"]['IdealTrader'] # We initially set it up with randomTraders
 
-    for _ in range(NUM_ORDERS_INIT):
-        market.usd_eth = 1.0 / get_eth_price(cur_time) 
+    for _ in range(market.params["NUM_ORDERS_INIT"]):
+        market.usd_eth = 1.0 / get_eth_price(cur_time, market)
         
         tid = randint(1, NUM_TRADERS)
-        orders = traderPool[tid].marketStep()
+        orders = market.traderPool[tid].marketStep()
         for order in orders:
             market.processOrder(order)
         cur_time += 1
         
-    # Keep demandRatio constant at 0.5 when we start the market    
+    # Keep demandRatio constant at 0.5 when we start the market
     market.demandRatio = 0.5
-    return jsonify(market.prices['wavg_fair'][100:])
+    
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def trade(market, trackers):
+    global cur_time
+    NUM_TRADERS = market.params["trader_demographics"]['AverageTrader'] + market.params["trader_demographics"]['BasicTrader']
+    OFFSET_TRADERS = market.params["trader_demographics"]['IdealTrader']
+    
+    for i in range(market.params["NUM_ORDERS_LIVE"]):
+        market.usd_eth = 1 / get_eth_price(cur_time, market) 
+        if i % market.params["TRACK_FREQ"] == 0:
+            track(trackers, market)
+            
+        tid = randint(OFFSET_TRADERS + 1, OFFSET_TRADERS + NUM_TRADERS)
+        orders = market.traderPool[tid].marketStep()
+        for order in orders:
+            market.processOrder(order)    
+        cur_time += 1
+
+
+def run(params, query):
+    params = Params(params)
+    market = Market(params)
+    basis = BasisProtocol(int(1e7), market)
+    market.protocol = basis
+    traderPool = createTraderPool(basis, market, market.params["trader_demographics"])
+    market.setTraderPool(traderPool)
+
+    cur_time = 0
+    market.usd_eth = 1 / get_eth_price(0, market)
+
+    trackers = {'usd_eth': [], 'bond_q_len': [], 'demand_ratio': [], 'ask_volume': [],
+                'bid_volume': [], 'total_volume': [], 'circulation': []}
+
+    warmup(market)
+
+    trade(market, trackers)
