b98c41abe609e6bbbd3c3657446e5e0047a3081e
Andrew Kirillov <kirillov.edu@gmail.com>
2020-05-06 00:50:17 -0400

    implemented streaming of simulation data via socket.io, added minimal frontend for debugging

--- demo/backend/flask_functions.py
+++ demo/backend/flask_functions.py
@@ -3,6 +3,18 @@ sys.path.append("/Users/andrew/projects/bab-stablecoin_sims_research/demo/backen
 
 from random import randint
 from simulator import *
+from flask import Flask, jsonify
+from flask_socketio import SocketIO, emit, send
+from eventlet import monkey_patch
+
+monkey_patch()
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'satoshi'
+app.debug = True
+app.host = 'localhost'
+
+socketio = SocketIO(app, cors_allowed_origins="*")
 
 def track(trackers, market):
     trackers['usd_eth'].append(market.usd_eth)
@@ -14,40 +26,47 @@ def track(trackers, market):
     trackers['circulation'].append(market.getCirculation())
 
 def warmup(market):
-    global cur_time
     NUM_TRADERS = market.params["trader_demographics"]['IdealTrader'] # We initially set it up with randomTraders
+    TRACK_FREQ = market.params["TRACK_FREQ"]
 
-    for _ in range(market.params["NUM_ORDERS_INIT"]):
-        market.usd_eth = 1.0 / get_eth_price(cur_time, market)
-        
+    for i in range(market.params["NUM_ORDERS_INIT"]):
+        market.usd_eth = 1.0 / get_eth_price(market.cur_time, market)
+        if i % TRACK_FREQ == 0:
+            emit("warmup", market.prices['wavg_fair'][-TRACK_FREQ:])
+            socketio.sleep(0.01)
+
         tid = randint(1, NUM_TRADERS)
         orders = market.traderPool[tid].marketStep()
         for order in orders:
             market.processOrder(order)
-        cur_time += 1
-        
+        market.cur_time += 1
+
     # Keep demandRatio constant at 0.5 when we start the market
     market.demandRatio = 0.5
     
 
 def trade(market, trackers):
-    global cur_time
     NUM_TRADERS = market.params["trader_demographics"]['AverageTrader'] + market.params["trader_demographics"]['BasicTrader']
     OFFSET_TRADERS = market.params["trader_demographics"]['IdealTrader']
-    
+    TRACK_FREQ = market.params["TRACK_FREQ"]
+
     for i in range(market.params["NUM_ORDERS_LIVE"]):
-        market.usd_eth = 1 / get_eth_price(cur_time, market) 
-        if i % market.params["TRACK_FREQ"] == 0:
+        market.usd_eth = 1 / get_eth_price(market.cur_time, market) 
+        if i % TRACK_FREQ == 0:
             track(trackers, market)
-            
+            emit("data", market.prices['MAday'][-TRACK_FREQ:])
+            socketio.sleep(0.01)
+
         tid = randint(OFFSET_TRADERS + 1, OFFSET_TRADERS + NUM_TRADERS)
         orders = market.traderPool[tid].marketStep()
         for order in orders:
             market.processOrder(order)    
-        cur_time += 1
+        market.cur_time += 1
+            
 
 
-def run(params, query):
+@socketio.on("run")
+def run(params):
     params = Params(params)
     market = Market(params)
     basis = BasisProtocol(int(1e7), market)
@@ -55,12 +74,21 @@ def run(params, query):
     traderPool = createTraderPool(basis, market, market.params["trader_demographics"])
     market.setTraderPool(traderPool)
 
-    cur_time = 0
     market.usd_eth = 1 / get_eth_price(0, market)
 
     trackers = {'usd_eth': [], 'bond_q_len': [], 'demand_ratio': [], 'ask_volume': [],
                 'bid_volume': [], 'total_volume': [], 'circulation': []}
 
+    TRACK_FREQ = market.params["TRACK_FREQ"]
+
     warmup(market)
 
+    emit("warmup", market.prices['wavg_fair'][-TRACK_FREQ:])
+    socketio.sleep(0.01)
+
     trade(market, trackers)
+
+    emit("data", market.prices['MAday'][-TRACK_FREQ:])
+    emit("trackers", trackers)
+
+socketio.run(app)
--- demo/frontend/src/App.js
+++ demo/frontend/src/App.js
@@ -1,26 +1,73 @@
 import React from 'react';
-import logo from './logo.svg';
+import io from 'socket.io-client';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const endpoint = "http://localhost:5000";
 
-export default App;
+const socket = io.connect(`${endpoint}`);
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    
+    this.state = {
+      warmupData: [],
+      marketData: [],
+      trackerData: {},
+    }
+
+    this.getWarmupData = this.getWarmupData.bind(this);
+    this.getMarketData = this.getMarketData.bind(this);
+    this.getTrackerData = this.getTrackerData.bind(this);
+  }
+
+  componentDidMount = () => {
+    this.getWarmupData();
+    this.getMarketData();
+    this.getTrackerData();
+  }
+  
+  getWarmupData = () => {
+    socket.on("warmup", warmupDataObj => {
+      const warmupData = Array.from(warmupDataObj);
+      this.setState({ warmupData: [...this.state.warmupData, ...warmupData] });
+    })
+  }
+  
+  getMarketData = () => {
+    socket.on("data", marketDataObj => {
+      const marketData = Array.from(marketDataObj);
+      this.setState({ marketData: [...this.state.marketData, ...marketData] });
+    })
+  }
+
+  getTrackerData = () => {
+    socket.on("trackers", trackerData => {
+      this.setState({ trackerData });
+    })
+  }
+  
+  onClick = () => {
+    socket.emit("run", {});
+    this.setState({ marketData: [] });
+  };
+  
+  render = () => {
+    const { marketData } = this.state;
+    const { warmupData } = this.state;
+
+    return(
+      <div>
+        <button onClick={() => this.onClick()}>Run Simulation</button>
+        <h1>Warmup Data:</h1>
+        <div className="data warmup">
+          {warmupData.toString()}
+        </div>
+        <h1>Full Data:</h1>
+        <div className="data market">
+          {marketData.toString()}
+        </div>
+      </div>
+    )
+  }
+}
