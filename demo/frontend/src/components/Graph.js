5e5c6367d06b7f610d9f693fff58837ceea05478
Andrew Kirillov <kirillov.edu@gmail.com>
2020-05-06 21:25:13 -0400

    tweaked design

--- demo/frontend/src/components/Graph.js
+++ demo/frontend/src/components/Graph.js
@@ -32,7 +32,7 @@ function Graph(props) {
             x1="0%" y1="0%" x2="0%" y2="100%"
           >
             <stop offset="0%"   stopColor="rgba(62, 81, 181, 0.5)"/>
-            <stop offset="50%" stopColor="rgba(62, 81, 181, 0)"/>
+            <stop offset="100%" stopColor="rgba(62, 81, 181, 0)"/>
           </linearGradient>
         </defs>
       </svg>
