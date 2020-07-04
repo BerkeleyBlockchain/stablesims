b615a95e361e05008c0255bfaeb85174d50202f2
Andrew Kirillov <kirillov.edu@gmail.com>
2020-07-03 17:32:48 -0700

    implemented flip tend+dent+deal, flap tend, & flop dent

--- dai_cadcad/policies.py
+++ dai_cadcad/policies.py
@@ -3,15 +3,14 @@ from constants import *
 from copy import deepcopy
 
 
+# Vat
+
 def make_join(eth, dai):
 
   def join(_params, substep, sH, s, **kwargs):
 
-    # Assert that we don't cross the ETH debt ceiling    
     debt_dai = s["eth_ilk"]["debt_dai"]
     assert (debt_dai + dai < ETH_LINE)
-
-    # Assert that this vault will be properly collateralized
     # TODO: Consider making the collateralization ratio assertion a helper function
     spot = s["eth_ilk"]["spot_rate"]
     assert (spot * eth >= dai)
@@ -34,11 +33,12 @@ def make_join(eth, dai):
   return join
 
 
+# Cat
+
 def make_bite(vault_id):
 
   def bite(_params, substep, sH, s, **kwargs):
 
-    # Assert that vault is undercollateralized
     eth = s["vat"][vault_id]["eth"]
     dai = s["vat"][vault_id]["dai"]
     spot = spot = s["eth_ilk"]["spot_rate"]
@@ -49,69 +49,150 @@ def make_bite(vault_id):
     flip_id = uuid4()
     stability_rate = s["eth_ilk"]["stability_rate"]
     debt_to_flip = dai * stability_rate
-    # TODO: How is the initial fixed ETH lot decided?
-    # Here, I assume it uses the current conversion rate
-    initial_lot = min(eth, debt_to_flip/spot)
-    new_auction = {
+    new_flip = {
       "flip_id": flip_id,
       "phase": "tend",
       "debt_to_flip": debt_to_flip,
       "vault": vault_id,
-      "lot_eth": initial_lot,
+      "lot_eth": eth,
       "bid_dai": 0,
       "bidder": "",
       "expiry": FLIP_TAU
     }
 
     new_flipper = deepcopy(s["flipper"])
-    new_flipper[flip_id] = new_auction
+    new_flipper[flip_id] = new_flip
 
     return { "flipper": new_flipper }
 
   return bite
 
 
-def make_resolve_flip_tend(flip_id):
-  
-  def resolve_flip_tend(_params, substep, sH, s, **kwargs):
+# Flipper
 
-    # Assert that flip is in tend phase
-    phase = s["flipper"][flip_id]["phase"]
+def make_flip_tend(flip_id, bid, keeper_id):
+
+  def flip_tend(_params, substep, sH, s, **kwargs):
+    
+    # TODO: Check that `flip` exists?
+    
+    flip = s["flipper"][flip_id]
+
+    # TODO: Check that `flip` has not expired?
+
+    phase = flip["phase"]
     assert (phase == "tend")
-    
-    # Assert that the debt has been covered
-    # TODO: Should we actually make this assertion?
-    debt_to_flip = s["flipper"][flip_id]["debt_to_flip"]
-    bid_dai = s["flipper"][flip_id]["bid_dai"]
-    assert (bid_dai >= debt_to_flip)
+    bid_dai = flip["bid_dai"]
+    assert (bid > bid_dai * FLIP_BEG)
+    # Not sure why we don't want to raise more, but this is from the smart contract
+    # https://github.com/makerdao/dss/blob/master/src/flip.sol
+    debt_to_flip = flip["debt_to_flip"]
+    assert (bid <= debt_to_flip)
 
-    # Reward winning bidder with lot & withdraw bid
-    bidder = s["flipper"][flip_id]["bidder"]
-    lot_eth = s["flipper"][flip_id]["lot_eth"]
-    new_keepers = deepcopy(s["keepers"])
-    new_keepers[bidder]["eth"] += lot_eth
-    new_keepers[bidder]["dai"] -= bid_dai
-    
-    # Withdraw lot from vault
-    vault_id = s["flipper"][flip_id]["vault"]
-    new_vat = deepcopy(s["vat"])
-    new_vat[vault_id]["eth"] -= lot_eth
-
-    # Move flip to dent phase
     new_flipper = deepcopy(s["flipper"])
-    new_flipper[flip_id]["phase"] = "dent"
-    new_flipper[flip_id]["debt_to_flip"] -= bid_dai
-    # TODO: How is new initial ETH lot decided?
-    new_flipper[flip_id]["lot_eth"] = 0
-    # TODO: How is new fixed DAU bid decided?
-    new_flipper[flip_id]["bid_dai"] = 0
+    new_flip = new_flipper[flip_id]
+    new_flip["bid_dai"] = bid
+    new_flip["bidder"] = keeper_id
+    if (bid == debt_to_flip):
+      new_flip["phase"] = "dent"
+    # TODO: Should timekeeping be handled here?
+    new_flip["expiry"] -= 1
 
-    return {
-      "keepers": new_keepers,
-      "vat": new_vat,
-      "flipper": new_flipper
-    }
-
+    return { "flipper": new_flipper }
+  
+  return flip_tend
 
 
+def make_flip_dent(flip_id, lot, keeper_id):
+
+  def flip_dent(_params, substep, sH, s, **kwargs):
+
+    flip = s["flipper"][flip_id]
+
+    lot_eth = flip["lot_eth"]
+    assert (lot < lot_eth * FLIP_BEG)
+
+    new_flipper = deepcopy(s["flipper"])
+    new_flip = new_flipper[flip_id]
+    new_flip["lot_eth"] = lot
+    new_flip["bidder"] = keeper_id
+    new_flip["expiry"] -= 1
+
+    return { "flipper": new_flipper }
+
+  return flip_dent
+
+
+def make_flip_deal(flip_id, keeper_id):
+
+  def flip_deal(_params, substep, sH, s, **kwargs):
+
+    flip = s["flipper"][flip_id]
+
+    expiry = flip["expiry"]
+    assert (expiry == 0)
+
+    # Move lot to bidder (remaining eth left in user vault)
+    new_vat = deepcopy(s["vat"])
+    new_vault = new_vat[flip["vault"]]
+    lot = flip["lot_eth"]
+    new_vault["eth"] -= lot
+    # TODO: Should vault cleanup logic be handled here?
+    new_keepers = deepcopy(s["keepers"])
+    new_keeper = new_keepers[flip["bidder"]]
+    new_keeper["eth"] += lot
+
+    # Move bid to vow surplus
+    new_vow = deepcopy(s["vow"])
+    bid = flip["bid_dai"]
+    new_vow["surplus_dai"] += bid
+    new_keeper["dai"] -= bid
+
+    return { "vat": new_vat, "keepers": new_keepers, "vow": new_vow }
+  
+  return flip_deal
+
+
+# Flapper
+
+def make_flap_tend(flap_id, bid, keeper_id):
+
+  def flap_tend(_params, substep, sH, s, **kwargs):
+    
+    flap = s["flapper"][flap_id]
+
+    bid_mkr = flap["bid_mkr"]
+    assert (bid > bid_mkr * FLAP_BEG)
+
+    new_flapper = deepcopy(s["flapper"])
+    new_flap = new_flapper[flap_id]
+    new_flap["bid_mkr"] = bid
+    new_flap["bidder"] = keeper_id
+    new_flap["expiry"] -= 1
+
+    return { "flapper": new_flapper }
+
+  return flap_tend
+
+
+# Flopper
+
+def make_flop_dent(flop_id, lot, keeper_id):
+
+  def flop_dent(_params, substep, sH, s, **kwargs):
+
+    flop = s["flopper"][flop_id]
+
+    lot_mkr = flop["lot_mkr"]
+    assert (lot < lot_mkr * FLOP_BEG)
+
+    new_flopper = deepcopy(s["flopper"])
+    new_flop = new_flopper[flop_id]
+    new_flop["lot_mkr"] = lot
+    new_flop["bidder"] = keeper_id
+    new_flop["expiry"] -= 1
+
+    return { "flopper": new_flopper }
+
+  return flop_dent
 
--- dai_cadcad/state.py
+++ dai_cadcad/state.py
@@ -1,9 +1,9 @@
 initial_state = {
   "vat": {
     "dummy_vault": {
-      "vault_id": "",             # Vault ID
-      "eth": 0,             # Vault collateralization (ETH)
-      "dai": 0,             # Vault debt (DAI)
+      "vault_id": "",         # Vault ID
+      "eth": 0,               # Vault collateralization (ETH)
+      "dai": 0,               # Vault debt (DAI)
     }
   },
   "vow": {
@@ -12,37 +12,37 @@ initial_state = {
   },
   "flipper": {                # Tend -> dent
     "dummy_flip": {
-      "flip_id": "",             # Auction ID
-      "phase": "",          # Auction phase ("tend" or "dent")
-      "debt_to_flip": 0,    # Desired amount of DAI to be raised from auction
-      "vault": "",          # Vault to send remaining ETH to after dent
-      "lot_eth": 0,         # Current lot (ETH)
-      "bid_dai": 0,         # Current bid (DAI)
-      "bidder": "",         # Current highest bidder
-      "expiry": 0,          # Auction expiration timestep
+      "flip_id": "",          # Auction ID
+      "phase": "",            # Auction phase ("tend" or "dent")
+      "debt_to_flip": 0,      # Desired amount of DAI to be raised from auction
+      "vault": "",            # Vault to send remaining ETH to after dent
+      "lot_eth": 0,           # Current lot (ETH)
+      "bid_dai": 0,           # Current bid (DAI)
+      "bidder": "",           # Current highest bidder
+      "expiry": 0,            # Auction expiration timestep
     }
   },
   "flopper": {                # Dent
     "dummy_flop": {
-      "id": "",             # Auction ID
-      "lot_mkr": 0,         # Current lot (MKR)
-      "bid_dai": 0,         # Current bid (DAI)
-      "bidder": "",         # Current highest bidder
-      "expiry": 0,          # Auction expiration timestep
+      "flop_id": "",          # Auction ID
+      "lot_mkr": 0,           # Current lot (MKR)
+      "bid_dai": 0,           # Current bid (DAI)
+      "bidder": "",           # Current highest bidder
+      "expiry": 0,            # Auction expiration timestep
     }
   },
   "flapper": {                # Tend
     "dummy_flap": {
-      "id": "",             # Auction ID
-      "lot_dai": 0,         # Current lot (DAI)
-      "bid_mkr": 0,         # Current bid (MKR)
-      "bidder": "",         # Current highest bidder
-      "expiry": 0,          # Auction expiration timestep
+      "flap_id": "",          # Auction ID
+      "lot_dai": 0,           # Current lot (DAI)
+      "bid_mkr": 0,           # Current bid (MKR)
+      "bidder": "",           # Current highest bidder
+      "expiry": 0,            # Auction expiration timestep
     }
   },
   "keepers": {
     "dummy_keeper": {
-      "id": "",               # Keeper ID
+      "keeper_id": "",        # Keeper ID
       "dai": 0,               # DAI balance
       "eth": 0,               # ETH balance
       "mkr": 0,               # MKR balance
