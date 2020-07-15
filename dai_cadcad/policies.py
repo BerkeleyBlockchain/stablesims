49bf06f0beb28242eac274b3edddc68681415039
Sehyun Chung <sehyun@berkeley.edu>
2020-07-14 22:26:14 -0400

    start setup for join baby sim

--- dai_cadcad/policies.py
+++ dai_cadcad/policies.py
@@ -1,5 +1,5 @@
 from uuid import uuid4
-from constants import *
+import constants
 from copy import deepcopy
 
 
@@ -7,27 +7,27 @@ from copy import deepcopy
 
 def make_join(eth, dai):
 
-  def join(_params, substep, sH, s, **kwargs):
+    def join(_params, substep, sH, s, **kwargs):
 
-    debt_dai = s["eth_ilk"]["debt_dai"]
-    assert (debt_dai + dai < ETH_LINE)
-    
-    # Create the new vault to be joined
-    vault_id = uuid4()
-    new_vault = {
-      "vault_id": vault_id,
-      "eth": eth,
-      "dai": dai
-    }
+        debt_dai = s["eth_ilk"]["debt_dai"]
+        assert (debt_dai + dai < constants.ETH_LINE)
 
-    # Pretty sure copying is necessary here
-    # TODO: Consider making a copy+merge helper function
-    new_vat = deepcopy(s["vat"])
-    new_vat[vault_id] = new_vault
+        # Create the new vault to be joined
+        vault_id = uuid4()
+        new_vault = {
+            "vault_id": vault_id,
+            "eth": eth,
+            "dai": dai
+        }
 
-    return { "vat": new_vat }
+        # Pretty sure copying is necessary here
+        # TODO: Consider making a copy+merge helper function
+        new_vat = deepcopy(s["vat"])
+        new_vat[vault_id] = new_vault
 
-  return join
+        return {"vat": new_vat}
+
+    return join
 
 # ---------------------------------------------
 
@@ -36,36 +36,35 @@ def make_join(eth, dai):
 
 def make_bite(vault_id):
 
-  def bite(_params, substep, sH, s, **kwargs):
+    def bite(_params, substep, sH, s, **kwargs):
 
-    eth = s["vat"][vault_id]["eth"]
-    dai = s["vat"][vault_id]["dai"]
-    spot_rate = s["eth_ilk"]["spot_rate"]
-    stability_rate = s["eth_ilk"]["stability_rate"]
-    debt_to_flip = dai * stability_rate
-    assert (spot_rate * eth >= debt_to_flip)
+        eth = s["vat"][vault_id]["eth"]
+        dai = s["vat"][vault_id]["dai"]
+        spot_rate = s["eth_ilk"]["spot_rate"]
+        stability_rate = s["eth_ilk"]["stability_rate"]
+        debt_to_flip = dai * stability_rate
+        assert (spot_rate * eth >= debt_to_flip)
 
+        # Create the Flipper auction to be kicked
+        # Adding it to the flipper is the same as kicking it
+        flip_id = uuid4()
+        new_flip = {
+            "flip_id": flip_id,
+            "phase": "tend",
+            "debt_to_flip": debt_to_flip,
+            "vault": vault_id,
+            "lot_eth": eth,
+            "bid_dai": 0,
+            "bidder": "",
+            "expiry": FLIP_TAU
+        }
 
-    # Create the Flipper auction to be kicked
-    # Adding it to the flipper is the same as kicking it
-    flip_id = uuid4()
-    new_flip = {
-      "flip_id": flip_id,
-      "phase": "tend",
-      "debt_to_flip": debt_to_flip,
-      "vault": vault_id,
-      "lot_eth": eth,
-      "bid_dai": 0,
-      "bidder": "",
-      "expiry": FLIP_TAU
-    }
+        new_flipper = deepcopy(s["flipper"])
+        new_flipper[flip_id] = new_flip
 
-    new_flipper = deepcopy(s["flipper"])
-    new_flipper[flip_id] = new_flip
+        return {"flipper": new_flipper}
 
-    return { "flipper": new_flipper }
-
-  return bite
+    return bite
 
 # ---------------------------------------------
 
@@ -74,94 +73,94 @@ def make_bite(vault_id):
 
 def make_flip_tend(flip_id, bid, keeper_id):
 
-  def flip_tend(_params, substep, sH, s, **kwargs):
-    
-    # TODO: Check that `flip` exists?
-    
-    flip = s["flipper"][flip_id]
+    def flip_tend(_params, substep, sH, s, **kwargs):
 
-    # TODO: Check that `flip` has not expired?
+        # TODO: Check that `flip` exists?
 
-    phase = flip["phase"]
-    assert (phase == "tend")
-    bid_dai = flip["bid_dai"]
-    assert (bid > bid_dai * FLIP_BEG)
-    # Not sure why we don't want to raise more, but this is from the smart contract
-    # https://github.com/makerdao/dss/blob/master/src/flip.sol
-    debt_to_flip = flip["debt_to_flip"]
-    assert (bid <= debt_to_flip)
+        flip = s["flipper"][flip_id]
 
-    new_flipper = deepcopy(s["flipper"])
-    new_flip = new_flipper[flip_id]
-    new_flip["bid_dai"] = bid
-    new_flip["bidder"] = keeper_id
-    if (bid == debt_to_flip):
-      new_flip["phase"] = "dent"
-    # TODO: Should timekeeping be handled here?
-    new_flip["expiry"] -= 1
+        # TODO: Check that `flip` has not expired?
 
-    return { "flipper": new_flipper }
-  
-  return flip_tend
+        phase = flip["phase"]
+        assert (phase == "tend")
+        bid_dai = flip["bid_dai"]
+        assert (bid > bid_dai * FLIP_BEG)
+        # Not sure why we don't want to raise more, but this is from the smart contract
+        # https://github.com/makerdao/dss/blob/master/src/flip.sol
+        debt_to_flip = flip["debt_to_flip"]
+        assert (bid <= debt_to_flip)
+
+        new_flipper = deepcopy(s["flipper"])
+        new_flip = new_flipper[flip_id]
+        new_flip["bid_dai"] = bid
+        new_flip["bidder"] = keeper_id
+        if (bid == debt_to_flip):
+            new_flip["phase"] = "dent"
+        # TODO: Should timekeeping be handled here?
+        new_flip["expiry"] -= 1
+
+        return {"flipper": new_flipper}
+
+    return flip_tend
 
 
 def make_flip_dent(flip_id, lot, keeper_id):
 
-  def flip_dent(_params, substep, sH, s, **kwargs):
+    def flip_dent(_params, substep, sH, s, **kwargs):
 
-    flip = s["flipper"][flip_id]
+        flip = s["flipper"][flip_id]
 
-    phase = flip["phase"]
-    assert (phase == "dent")
+        phase = flip["phase"]
+        assert (phase == "dent")
 
-    lot_eth = flip["lot_eth"]
-    assert (lot < lot_eth * FLIP_BEG)
+        lot_eth = flip["lot_eth"]
+        assert (lot < lot_eth * FLIP_BEG)
 
-    new_flipper = deepcopy(s["flipper"])
-    new_flip = new_flipper[flip_id]
-    new_flip["lot_eth"] = lot
-    new_flip["bidder"] = keeper_id
-    new_flip["expiry"] -= 1
+        new_flipper = deepcopy(s["flipper"])
+        new_flip = new_flipper[flip_id]
+        new_flip["lot_eth"] = lot
+        new_flip["bidder"] = keeper_id
+        new_flip["expiry"] -= 1
 
-    return { "flipper": new_flipper }
+        return {"flipper": new_flipper}
 
-  return flip_dent
+    return flip_dent
 
 
 def make_flip_deal(flip_id):
 
-  def flip_deal(_params, substep, sH, s, **kwargs):
+    def flip_deal(_params, substep, sH, s, **kwargs):
 
-    flip = s["flipper"][flip_id]
+        flip = s["flipper"][flip_id]
 
-    expiry = flip["expiry"]
-    assert (expiry == 0)
+        expiry = flip["expiry"]
+        assert (expiry == 0)
 
-    # Move DAI bid from Keeper to Vow surplus
-    new_keepers = deepcopy(s["keepers"])
-    new_keeper = new_keepers[flip["bidder"]]
-    bid_dai = flip["bid_dai"]
-    new_keeper["dai"] -= bid_dai
-    new_vow = deepcopy(s["vow"])
-    new_vow["surplus_dai"] += bid_dai
+        # Move DAI bid from Keeper to Vow surplus
+        new_keepers = deepcopy(s["keepers"])
+        new_keeper = new_keepers[flip["bidder"]]
+        bid_dai = flip["bid_dai"]
+        new_keeper["dai"] -= bid_dai
+        new_vow = deepcopy(s["vow"])
+        new_vow["surplus_dai"] += bid_dai
 
-    # If not enough Dai raised, put remainder of debt in Vow
+        # If not enough Dai raised, put remainder of debt in Vow
 
-    # Move ETH lot from Vault to Keeper
-    new_vat = deepcopy(s["vat"])
-    new_vault = new_vat[flip["vault"]]
-    lot_eth = flip["lot_eth"]
-    new_vault["eth"] -= lot_eth
-    # TODO: Should vault cleanup logic be handled here?
-    new_keeper["eth"] += lot_eth
+        # Move ETH lot from Vault to Keeper
+        new_vat = deepcopy(s["vat"])
+        new_vault = new_vat[flip["vault"]]
+        lot_eth = flip["lot_eth"]
+        new_vault["eth"] -= lot_eth
+        # TODO: Should vault cleanup logic be handled here?
+        new_keeper["eth"] += lot_eth
 
-    # Delete Flipper auction
-    new_flipper = deepcopy(s["flipper"])
-    del new_flipper[flip_id]
+        # Delete Flipper auction
+        new_flipper = deepcopy(s["flipper"])
+        del new_flipper[flip_id]
 
-    return {  "vow": new_vow, "keepers": new_keepers, "vat": new_vat, "flipper": new_flipper }
-  
-  return flip_deal
+        return {"vow": new_vow, "keepers": new_keepers, "vat": new_vat, "flipper": new_flipper}
+
+    return flip_deal
 
 # ---------------------------------------------
 
@@ -170,52 +169,52 @@ def make_flip_deal(flip_id):
 
 def make_flap_tend(flap_id, bid, keeper_id):
 
-  def flap_tend(_params, substep, sH, s, **kwargs):
-    
-    flap = s["flapper"][flap_id]
+    def flap_tend(_params, substep, sH, s, **kwargs):
 
-    bid_mkr = flap["bid_mkr"]
-    assert (bid > bid_mkr * FLAP_BEG)
+        flap = s["flapper"][flap_id]
 
-    new_flapper = deepcopy(s["flapper"])
-    new_flap = new_flapper[flap_id]
-    new_flap["bid_mkr"] = bid
-    new_flap["bidder"] = keeper_id
-    new_flap["expiry"] -= 1
+        bid_mkr = flap["bid_mkr"]
+        assert (bid > bid_mkr * FLAP_BEG)
 
-    return { "flapper": new_flapper }
+        new_flapper = deepcopy(s["flapper"])
+        new_flap = new_flapper[flap_id]
+        new_flap["bid_mkr"] = bid
+        new_flap["bidder"] = keeper_id
+        new_flap["expiry"] -= 1
 
-  return flap_tend
+        return {"flapper": new_flapper}
+
+    return flap_tend
 
 
 def make_flap_deal(flap_id):
 
-  def flap_deal(_params, substep, sH, s, **kwargs):
+    def flap_deal(_params, substep, sH, s, **kwargs):
 
-    flap = s["flapper"][flap_id]
+        flap = s["flapper"][flap_id]
 
-    expiry = flap["expiry"]
-    assert (expiry == 0)
+        expiry = flap["expiry"]
+        assert (expiry == 0)
 
-    # Burn Keeper's MKR bid
-    bid_mkr = flap["bid_mkr"]
-    new_keepers = deepcopy(s["keepers"])
-    new_keeper = new_keepers[flap["bidder"]]
-    new_keeper["mkr"] -= bid_mkr
+        # Burn Keeper's MKR bid
+        bid_mkr = flap["bid_mkr"]
+        new_keepers = deepcopy(s["keepers"])
+        new_keeper = new_keepers[flap["bidder"]]
+        new_keeper["mkr"] -= bid_mkr
 
-    # Move DAI lot from Vow surplus to Keeper
-    new_vow = deepcopy(s["vow"])
-    lot_dai = flap["lot_dai"]
-    new_vow["surplus_dai"] -= lot_dai
-    new_keeper["dai"] += lot_dai
+        # Move DAI lot from Vow surplus to Keeper
+        new_vow = deepcopy(s["vow"])
+        lot_dai = flap["lot_dai"]
+        new_vow["surplus_dai"] -= lot_dai
+        new_keeper["dai"] += lot_dai
 
-    # Delete Flapper auction
-    new_flapper = deepcopy(s["flapper"])
-    del new_flapper[flap_id]
+        # Delete Flapper auction
+        new_flapper = deepcopy(s["flapper"])
+        del new_flapper[flap_id]
 
-    return { "keepers": new_keepers, "vow": new_vow, "flapper": new_flapper }
-  
-  return flap_deal
+        return {"keepers": new_keepers, "vow": new_vow, "flapper": new_flapper}
+
+    return flap_deal
 
 # ---------------------------------------------
 
@@ -224,51 +223,51 @@ def make_flap_deal(flap_id):
 
 def make_flop_dent(flop_id, lot, keeper_id):
 
-  def flop_dent(_params, substep, sH, s, **kwargs):
+    def flop_dent(_params, substep, sH, s, **kwargs):
 
-    flop = s["flopper"][flop_id]
+        flop = s["flopper"][flop_id]
 
-    lot_mkr = flop["lot_mkr"]
-    assert (lot < lot_mkr * FLOP_BEG)
+        lot_mkr = flop["lot_mkr"]
+        assert (lot < lot_mkr * FLOP_BEG)
 
-    new_flopper = deepcopy(s["flopper"])
-    new_flop = new_flopper[flop_id]
-    new_flop["lot_mkr"] = lot
-    new_flop["bidder"] = keeper_id
-    new_flop["expiry"] -= 1
+        new_flopper = deepcopy(s["flopper"])
+        new_flop = new_flopper[flop_id]
+        new_flop["lot_mkr"] = lot
+        new_flop["bidder"] = keeper_id
+        new_flop["expiry"] -= 1
 
-    return { "flopper": new_flopper }
+        return {"flopper": new_flopper}
 
-  return flop_dent
+    return flop_dent
 
 
 def make_flop_deal(flop_id):
 
-  def flop_deal(_params, substep, sH, s, **kwargs):
+    def flop_deal(_params, substep, sH, s, **kwargs):
 
-    flop = s["flopper"][flop_id]
+        flop = s["flopper"][flop_id]
 
-    expiry = flop["expiry"]
-    assert (expiry == 0)
+        expiry = flop["expiry"]
+        assert (expiry == 0)
 
-    # Move DAI bid from Keeper to Vow surplus
-    new_keepers = deepcopy(s["keepers"])
-    new_keeper = new_keepers[flop["bidder"]]
-    bid_dai = flop["bid_dai"]
-    new_keeper["dai"] -= bid_dai
-    new_vow = deepcopy(s["vow"])
-    new_vow["surplus_dai"] += bid_dai
+        # Move DAI bid from Keeper to Vow surplus
+        new_keepers = deepcopy(s["keepers"])
+        new_keeper = new_keepers[flop["bidder"]]
+        bid_dai = flop["bid_dai"]
+        new_keeper["dai"] -= bid_dai
+        new_vow = deepcopy(s["vow"])
+        new_vow["surplus_dai"] += bid_dai
 
-    # Mint MKR lot
-    lot_mkr = flop["lot_mkr"]
-    new_keeper["mkr"] += lot_mkr
+        # Mint MKR lot
+        lot_mkr = flop["lot_mkr"]
+        new_keeper["mkr"] += lot_mkr
 
-    # Delete Flopper auction
-    new_flopper = deepcopy(s["flopper"])
-    del new_flopper[flop_id]
+        # Delete Flopper auction
+        new_flopper = deepcopy(s["flopper"])
+        del new_flopper[flop_id]
 
-    return { "vow": new_vow, "keepers": new_keepers, "flopper": new_flopper }
-  
-  return flap_deal
+        return {"vow": new_vow, "keepers": new_keepers, "flopper": new_flopper}
+
+    return flap_deal
 
 # ---------------------------------------------
--- dai_cadcad/state.py
+++ dai_cadcad/state.py
@@ -1,94 +1,102 @@
 initial_state = {
-  "vat": {
-    "dummy_vault": {
-      "vault_id": "",         # Vault ID
-      "eth": 0,               # Vault collateralization (ETH)
-      "dai": 0,               # Vault debt (DAI)
-    }
-  },
-  "vow": {
-    "debt_dai": 0,            # System debt (DAI)
-    "surplus_dai": 0,         # System surplus (DAI)
-  },
-  "flipper": {                # Tend -> dent
-    "dummy_flip": {
-      "flip_id": "",          # Auction ID
-      "phase": "",            # Auction phase ("tend" or "dent")
-      "debt_to_flip": 0,      # Desired amount of DAI to be raised from auction
-      "vault": "",            # Vault to send remaining ETH to after dent
-      "lot_eth": 0,           # Current lot (ETH)
-      "bid_dai": 0,           # Current bid (DAI)
-      "bidder": "",           # Current highest bidder
-      "expiry": 0,            # Auction expiration timestep
-    }
-  },
-  "flapper": {                # Tend
-    "dummy_flap": {
-      "flap_id": "",          # Auction ID
-      "lot_dai": 0,           # Current lot (DAI)
-      "bid_mkr": 0,           # Current bid (MKR)
-      "bidder": "",           # Current highest bidder
-      "expiry": 0,            # Auction expiration timestep
-    }
-  },
-  "flopper": {                # Dent
-    "dummy_flop": {
-      "flop_id": "",          # Auction ID
-      "lot_mkr": 0,           # Current lot (MKR)
-      "bid_dai": 0,           # Current bid (DAI)
-      "bidder": "",           # Current highest bidder
-      "expiry": 0,            # Auction expiration timestep
-    }
-  },
-  "keepers": {
-    "dummy_keeper": {
-      "keeper_id": "",        # Keeper ID
-      "dai": 0,               # DAI balance
-      "eth": 0,               # ETH balance
-      "mkr": 0,               # MKR balance
-      "flip_tolerance": 0,    # (TODO)
-      "flop_tolerance": 0,    # (TODO)
-      "flap_tolerance": 0     # (TODO)
-    }
-  },
-  "eth_ilk": {
-    "debt_dai": 0,            # Total amount of DAI collateralized by ETH
-    "spot_rate": 0,           # Conversion rate (max amount of DAI per ETH)
-    "stability_rate": 0       # Stability fee rate
-  },
+    "vat": {
+        "dummy_vault": {
+            "vault_id": "",         # Vault ID
+            "eth": 0,               # Vault collateralization (ETH)
+            "dai": 0,               # Vault debt (DAI)
+        }
+    },
+    "vow": {
+        "debt_dai": 0,            # System debt (DAI)
+        "surplus_dai": 0,         # System surplus (DAI)
+    },
+    "flipper": {                # Tend -> dent
+        "dummy_flip": {
+            "flip_id": "",          # Auction ID
+            "phase": "",            # Auction phase ("tend" or "dent")
+            "debt_to_flip": 0,      # Desired amount of DAI to be raised from auction
+            "vault": "",            # Vault to send remaining ETH to after dent
+            "lot_eth": 0,           # Current lot (ETH)
+            "bid_dai": 0,           # Current bid (DAI)
+            "bidder": "",           # Current highest bidder
+            "expiry": 0,            # Auction expiration timestep
+        }
+    },
+    "flapper": {                # Tend
+        "dummy_flap": {
+            "flap_id": "",          # Auction ID
+            "lot_dai": 0,           # Current lot (DAI)
+            "bid_mkr": 0,           # Current bid (MKR)
+            "bidder": "",           # Current highest bidder
+            "expiry": 0,            # Auction expiration timestep
+        }
+    },
+    "flopper": {                # Dent
+        "dummy_flop": {
+            "flop_id": "",          # Auction ID
+            "lot_mkr": 0,           # Current lot (MKR)
+            "bid_dai": 0,           # Current bid (DAI)
+            "bidder": "",           # Current highest bidder
+            "expiry": 0,            # Auction expiration timestep
+        }
+    },
+    "keepers": {
+        "dummy_keeper": {
+            "keeper_id": "",        # Keeper ID
+            "dai": 0,               # DAI balance
+            "eth": 0,               # ETH balance
+            "mkr": 0,               # MKR balance
+            "flip_tolerance": 0,    # (TODO)
+            "flop_tolerance": 0,    # (TODO)
+            "flap_tolerance": 0     # (TODO)
+        }
+    },
+    "eth_ilk": {
+        "debt_dai": 0,            # Total amount of DAI collateralized by ETH
+        "spot_rate": 0,           # Conversion rate (max amount of DAI per ETH)
+        "stability_rate": 0       # Stability fee rate
+    },
 }
 
+
 def update_vat(_params, substep, sH, s, _input, **kwargs):
-  curr_vat = s["vat"]
-  # Set new_vat
-  return ("vat", new_vat)
+    curr_vat = s["vat"]
+    # Set new_vat
+    new_vat = _input['vat']
+    return ("vat", new_vat)
 
-def update_vow(_params, substep, sH, s, _input, **kwargs):
-  curr_vow = s["vow"]
-  # Set new_vow
-  return ("vow", new_vow)
 
-def update_flipper(_params, substep, sH, s, _input, **kwargs):
-  curr_flipper = s["flipper"]
-  # Set new_flipper
-  return ("flipper", new_flipper)
+# def update_vow(_params, substep, sH, s, _input, **kwargs):
+#     curr_vow = s["vow"]
+#     # Set new_vow
+#     return ("vow", new_vow)
 
-def update_flopper(_params, substep, sH, s, _input, **kwargs):
-  curr_flopper = s["flopper"]
-  # Set new_flopper
-  return ("flopper", new_flopper)
 
-def update_flapper(_params, substep, sH, s, _input, **kwargs):
-  curr_flapper = s["flapper"]
-  # Set new_flapper
-  return ("flapper", new_flapper)
+# def update_flipper(_params, substep, sH, s, _input, **kwargs):
+#     curr_flipper = s["flipper"]
+#     # Set new_flipper
+#     return ("flipper", new_flipper)
 
-def update_keepers(_params, substep, sH, s, _input, **kwargs):
-  curr_keepers = s["keepers"]
-  # Set new_keepers
-  return ("keepers", new_keepers)
 
-def update_eth_ilk(_params, substep, sH, s, _input, **kwargs):
-  curr_eth_ilk = s["eth_ilk"]
-  # Set new_eth_ilk
-  return ("eth_ilk", eth_ilk)
+# def update_flopper(_params, substep, sH, s, _input, **kwargs):
+#     curr_flopper = s["flopper"]
+#     # Set new_flopper
+#     return ("flopper", new_flopper)
+
+
+# def update_flapper(_params, substep, sH, s, _input, **kwargs):
+#     curr_flapper = s["flapper"]
+#     # Set new_flapper
+#     return ("flapper", new_flapper)
+
+
+# def update_keepers(_params, substep, sH, s, _input, **kwargs):
+#     curr_keepers = s["keepers"]
+#     # Set new_keepers
+#     return ("keepers", new_keepers)
+
+
+# def update_eth_ilk(_params, substep, sH, s, _input, **kwargs):
+#     curr_eth_ilk = s["eth_ilk"]
+#     # Set new_eth_ilk
+#     return ("eth_ilk", eth_ilk)
