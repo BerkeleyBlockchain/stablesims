a9493c645a3194838390800b7cd9ee273157d945
Andrew Kirillov <kirillov.edu@gmail.com>
2020-07-23 23:44:29 -0400

    final lint fix

--- dai_cadcad/sim_configs.py
+++ dai_cadcad/sim_configs.py
@@ -1,46 +1,59 @@
+""" Simulation Configurations Module
+
+Contains cadCAD configuration objects for different simulations, along with a dummy object to
+inherit from/reference.
+"""
+
 from cadCAD.configuration.utils import config_sim
 
 dummy_sim_config = {
     "T": range(1),
     "N": 1,
     "M": {
-        "SUMP": [0],        # Fixed DAI bid for Flopper auctions
-        "DUMP": [0],        # Initial MKR lot for Flopper auctions
-        "BUMP": [0],        # Fixed DAI lot for Flapper auctions
-        "HUMP": [0],        # Surplus DAI threshold that enables Flapper auctions
-        "FLIP_BEG": [0],    # Minimum DAI bid increase
-        "FLIP_TAU": [0],    # Auction duration
-        "FLOP_BEG": [0],    # Minimum ETH lot decrease
-        "FLOP_TAU": [0],    # Auction duration
-        "FLAP_BEG": [0],    # Minimum MKR bid increase
-        "FLAP_TAU": [0],    # Auction duration
-        "ETH_LINE": [0],    # ETH debt ceiling
-        "ETH_DUST": [0],    # ETH debt floor
-        "DSR": [0],         # Dai savings rate
-        "WARM_TAU": [0],    # Vault-joining warmup duration
-    }
+        "SUMP": [0],  # Fixed DAI bid for Flopper auctions
+        "DUMP": [0],  # Initial MKR lot for Flopper auctions
+        "BUMP": [0],  # Fixed DAI lot for Flapper auctions
+        "HUMP": [0],  # Surplus DAI threshold that enables Flapper auctions
+        "FLIP_BEG": [0],  # Minimum DAI bid increase
+        "FLIP_TAU": [0],  # Auction duration
+        "FLOP_BEG": [0],  # Minimum ETH lot decrease
+        "FLOP_TAU": [0],  # Auction duration
+        "FLAP_BEG": [0],  # Minimum MKR bid increase
+        "FLAP_TAU": [0],  # Auction duration
+        "ETH_LINE": [0],  # ETH debt ceiling
+        "ETH_DUST": [0],  # ETH debt floor
+        "DSR": [0],  # Dai savings rate
+        "WARM_TAU": [0],  # Vault-joining warmup duration
+    },
 }
 
 # Note: at least one of the array fields w/in the "M" key should be of length > 1
-# Otherwise, the `_params` dict gets passed in as a 1-element array to state update and policy functions
+
+# Otherwise, the `_params` dict gets passed in as a 1-element array to state update and policy
+# functions
+
 # This is likely a cadCAD bug
 
-join_bite_sim_config = config_sim({
-  **dummy_sim_config,
-  **{ "M": {
-        "SUMP": [0],
-        "DUMP": [0],
-        "BUMP": [0],
-        "HUMP": [0],
-        "FLIP_BEG": [0],
-        "FLIP_TAU": [0],
-        "FLOP_BEG": [0],
-        "FLOP_TAU": [0],
-        "FLAP_BEG": [0],
-        "FLAP_TAU": [0],
-        "ETH_LINE": [0, 160000],
-        "ETH_DUST": [0],
-        "DSR": [0],
-        "WARM_TAU": [0, 1],
-  }}
-})
+join_bite_sim_config = config_sim(
+    {
+        **dummy_sim_config,
+        **{
+            "M": {
+                "SUMP": [0],
+                "DUMP": [0],
+                "BUMP": [0],
+                "HUMP": [0],
+                "FLIP_BEG": [0],
+                "FLIP_TAU": [0],
+                "FLOP_BEG": [0],
+                "FLOP_TAU": [0],
+                "FLAP_BEG": [0],
+                "FLAP_TAU": [0],
+                "ETH_LINE": [0, 160000],
+                "ETH_DUST": [0],
+                "DSR": [0],
+                "WARM_TAU": [0, 1],
+            }
+        },
+    }
+)
--- dai_cadcad/state.py
+++ dai_cadcad/state.py
@@ -1,95 +1,117 @@
+""" State Module
+
+Contains the definition of the cadCAD system's state variables, along with dummy data as an
+expected schema.
+Additionally, it contains primitive state update functions (all the real logic is handled by the
+policies).
+"""
+
 initial_state = {
-  "vat": {
-    "dummy_vault": {
-      "vault_id": "",         # Vault ID
-      "eth": 0,               # Vault collateralization (ETH)
-      "dai": 0,               # Vault debt (DAI)
-      "bitten": False         # Whether or not the vault has been bitten
-    }
-  },
-  "vow": {
-    "debt_dai": 0,            # System debt (DAI)
-    "surplus_dai": 0,         # System surplus (DAI)
-  },
-  "flipper": {                # Tend -> dent
-    "dummy_flip": {
-      "flip_id": "",          # Auction ID
-      "phase": "",            # Auction phase ("tend" or "dent")
-      "debt_to_flip": 0,      # Desired amount of DAI to be raised from auction
-      "vault": "",            # Vault to send remaining ETH to after dent
-      "lot_eth": 0,           # Current lot (ETH)
-      "bid_dai": 0,           # Current bid (DAI)
-      "bidder": "",           # Current highest bidder
-      "expiry": 0,            # Auction expiration timestep
-    }
-  },
-  "flapper": {                # Tend
-    "dummy_flap": {
-      "flap_id": "",          # Auction ID
-      "lot_dai": 0,           # Current lot (DAI)
-      "bid_mkr": 0,           # Current bid (MKR)
-      "bidder": "",           # Current highest bidder
-      "expiry": 0,            # Auction expiration timestep
-    }
-  },
-  "flopper": {                # Dent
-    "dummy_flop": {
-      "flop_id": "",          # Auction ID
-      "lot_mkr": 0,           # Current lot (MKR)
-      "bid_dai": 0,           # Current bid (DAI)
-      "bidder": "",           # Current highest bidder
-      "expiry": 0,            # Auction expiration timestep
-    }
-  },
-  "keepers": {
-    "dummy_keeper": {
-      "keeper_id": "",        # Keeper ID
-      "dai": 0,               # DAI balance
-      "eth": 0,               # ETH balance
-      "mkr": 0,               # MKR balance
-      "flip_tolerance": 0,    # (TODO)
-      "flop_tolerance": 0,    # (TODO)
-      "flap_tolerance": 0     # (TODO)
-    }
-  },
-  "eth_ilk": {
-    "debt_dai": 0,            # Total amount of DAI collateralized by ETH
-    "spot_rate": 2/3,         # Conversion rate (max amount of DAI per ETH)
-    "stability_rate": 1       # Stability fee rate
-  },
+    "vat": {
+        "dummy_vault": {
+            "vault_id": "",  # Vault ID
+            "eth": 0,  # Vault collateralization (ETH)
+            "dai": 0,  # Vault debt (DAI)
+            "bitten": False,  # Whether or not the vault has been bitten
+        }
+    },
+    "vow": {
+        "debt_dai": 0,  # System debt (DAI)
+        "surplus_dai": 0,  # System surplus (DAI)
+    },
+    "flipper": {  # Tend -> dent
+        "dummy_flip": {
+            "flip_id": "",  # Auction ID
+            "phase": "",  # Auction phase ("tend" or "dent")
+            "debt_to_flip": 0,  # Desired amount of DAI to be raised from auction
+            "vault": "",  # Vault to send remaining ETH to after dent
+            "lot_eth": 0,  # Current lot (ETH)
+            "bid_dai": 0,  # Current bid (DAI)
+            "bidder": "",  # Current highest bidder
+            "expiry": 0,  # Auction expiration timestep
+        }
+    },
+    "flapper": {  # Tend
+        "dummy_flap": {
+            "flap_id": "",  # Auction ID
+            "lot_dai": 0,  # Current lot (DAI)
+            "bid_mkr": 0,  # Current bid (MKR)
+            "bidder": "",  # Current highest bidder
+            "expiry": 0,  # Auction expiration timestep
+        }
+    },
+    "flopper": {  # Dent
+        "dummy_flop": {
+            "flop_id": "",  # Auction ID
+            "lot_mkr": 0,  # Current lot (MKR)
+            "bid_dai": 0,  # Current bid (DAI)
+            "bidder": "",  # Current highest bidder
+            "expiry": 0,  # Auction expiration timestep
+        }
+    },
+    "keepers": {
+        "dummy_keeper": {
+            "keeper_id": "",  # Keeper ID
+            "dai": 0,  # DAI balance
+            "eth": 0,  # ETH balance
+            "mkr": 0,  # MKR balance
+            "flip_tolerance": 0,  # (TODO)
+            "flop_tolerance": 0,  # (TODO)
+            "flap_tolerance": 0,  # (TODO)
+        }
+    },
+    "eth_ilk": {
+        "debt_dai": 0,  # Total amount of DAI collateralized by ETH
+        "spot_rate": 2 / 3,  # Conversion rate (max amount of DAI per ETH)
+        "stability_rate": 1,  # Stability fee rate
+    },
 }
 
 
-def update_vat(_params, substep, sH, s, _input, **kwargs):
-    new_vat = _input['vat']
+def update_vat(_params, _substep, _state_hist, _state, policy_signals):
+    """ Updates `vat` state variable.
+    """
+    new_vat = policy_signals["vat"]
     return ("vat", new_vat)
 
 
-def update_vow(_params, substep, sH, s, _input, **kwargs):
-    new_vow = _input["vow"]
+def update_vow(_params, _substep, _state_hist, _state, policy_signals):
+    """ Updates `vow` state variable.
+    """
+    new_vow = policy_signals["vow"]
     return ("vow", new_vow)
 
 
-def update_flipper(_params, substep, sH, s, _input, **kwargs):
-    new_flipper = _input["flipper"]
+def update_flipper(_params, _substep, _state_hist, _state, policy_signals):
+    """ Updates `flipper` state variable.
+    """
+    new_flipper = policy_signals["flipper"]
     return ("flipper", new_flipper)
 
 
-def update_flopper(_params, substep, sH, s, _input, **kwargs):
-    new_flopper = _input["flopper"]
+def update_flopper(_params, _substep, _state_hist, _state, policy_signals):
+    """ Updates `flopper` state variable.
+    """
+    new_flopper = policy_signals["flopper"]
     return ("flopper", new_flopper)
 
 
-def update_flapper(_params, substep, sH, s, _input, **kwargs):
-    new_flapper = _input["flapper"]
+def update_flapper(_params, _substep, _state_hist, _state, policy_signals):
+    """ Updates `flapper` state variable.
+    """
+    new_flapper = policy_signals["flapper"]
     return ("flapper", new_flapper)
 
 
-def update_keepers(_params, substep, sH, s, _input, **kwargs):
-    new_keepers = _input["keepers"]
+def update_keepers(_params, _substep, _state_hist, _state, policy_signals):
+    """ Updates `keepers` state variable.
+    """
+    new_keepers = policy_signals["keepers"]
     return ("keepers", new_keepers)
 
 
-def update_eth_ilk(_params, substep, sH, s, _input, **kwargs):
-    new_eth_ilk = _input["eth_ilk"]
+def update_eth_ilk(_params, _substep, _state_hist, _state, policy_signals):
+    """ Updates `eth_ilk` state variable.
+    """
+    new_eth_ilk = policy_signals["eth_ilk"]
     return ("eth_ilk", new_eth_ilk)
