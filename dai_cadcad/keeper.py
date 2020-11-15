2c9ecaf7f34c9afb0fb17e930f115e4b3fd4a7b6
Andrew Kirillov <kirillov.edu@gmail.com>
2020-11-15 12:54:48 -0500

    "merged" andrew/keeper-oop-refactor

--- dai_cadcad/keeper.py
+++ dai_cadcad/keeper.py
@@ -6,7 +6,7 @@
 
 from uuid import uuid4
 
-from dai_cadcad.pymaker.numeric import Wad, Rad  # , Ray
+from dai_cadcad.pymaker.numeric import Wad  # , Rad, Ray
 
 # from dai_cadcad.util import require
 
@@ -16,48 +16,86 @@ class Keeper:
 
     vat = None
     dai_join = None
+    gem_joins = {}
+    flippers = {}
 
-    # The collateral balances held by this Keeper outside of the Maker Protocol.
-    ilks = {}
-    # The ERC-20 DAI balance held by this Keeper
-    dai = Rad(0)
-    # The CDPs held by this Keeper (same ID used for urn, gem, & dai addresses)
-    cdps = {}
+    urns = {}
+    num_urns = 0
 
-    def __init__(self, ilks, vat, dai_join):
-        """ `ilks` must be an array containing a configuration abject for each ilk type of the
-            following form:
-                {
-                    "ilk_id": str,
-                    "gem": Wad,
-                    "flip": Flipper,
-                    "join" GemJoin,
-                }
-        """
+    def __init__(self, vat, dai_join, ilks):
 
-        self.ADDRESS = uuid4().hex
+        self.ADDRESS = f"keeper-{uuid4().hex}"
 
         self.vat = vat
         self.dai_join = dai_join
 
         for ilk in ilks:
-            self.cdps[ilk["ilk_id"]] = []
-            self.ilks[ilk["ilk_id"]] = {
-                "gem": ilk["gem"],
-                "flip": ilk["flip"],
-                "join": ilk["join"],
-            }
+            self.gem_joins[ilk["ilk_id"]] = ilk["gem_join"]
+            self.flippers[ilk["ilk_id"]] = ilk["flipper"]
 
-    def open_vault(self, ilk_id, gem, dai):
-        cdp_id = uuid4().hex
-        # TOOD: Make the `GemJoin` contract handle checking that this Keeper has enough `gem`.
-        # TODO: Have the `join` method expect the sender address instead of `msg.sender`
-        self.ilks[ilk_id]["join"].join(self.ADDRESS, cdp_id, gem)
-        self.cdps[ilk_id].append(cdp_id)
-        self.vat.frob(ilk_id, cdp_id, cdp_id, cdp_id, gem, dai)
+    def generate_urn_key(self):
+        urn_key = f"urn-{self.num_urns}-{self.ADDRESS}"
+        self.num_urns += 1
+        return urn_key
 
-    def close_vault(self, ilk_id, cdp_id):
-        dink = Wad(0) - self.vat.urns[ilk_id][cdp_id]["ink"]
-        dart = Wad(0) - self.vat.urns[ilk_id][cdp_id]["art"]
-        self.vat.frob(ilk_id, cdp_id, cdp_id, cdp_id, dink, dart)
-        self.ilks[ilk_id]["join"].exit(self.ADDRESS, cdp_id, Wad(0) - dink)
+    def open_vault(self, ilk_id, dink, dart):
+        urn_key = self.generate_urn_key()
+        self.urns[urn_key] = ilk_id
+        # Lock collateral into the system
+        self.gem_joins[ilk_id].join(self.ADDRESS, self.ADDRESS, dink)
+        # Create a vault with the locked collateral
+        # `vat.gem` and `vat.dai` use the Keeper's address, while `vat.urns` uses `urn_key`
+        self.vat.frob(ilk_id, urn_key, self.ADDRESS, self.ADDRESS, dink, dart)
+        # TODO: Ensure that `vat.move` can create a recipient address if it doesn't exist
+        # Move in-system DAI from the urn address to the keeper address
+        self.vat.move(urn_key, self.ADDRESS)
+
+    def close_vault(self, urn_key):
+        ilk_id = self.urns[urn_key]
+        dink = Wad(0) - self.vat.urns[ilk_id][urn_key]["ink"]
+        dart = Wad(0) - self.vat.urns[ilk_id][urn_key]["art"]
+        # Zero out the vault, freeing collateral
+        self.vat.frob(ilk_id, urn_key, self.ADDRESS, self.ADDRESS, dink, dart)
+        # Withdraw freed collateral
+        self.gem_joins[ilk_id].exit(self.ADDRESS, self.ADDRESS, Wad(0) - dink)
+        del self.urns[urn_key]
+
+    # def keeper_bid_flipper_eth(
+    #     self, flipper, vat, spotter, stance, bid_id, user_id, now, stats
+    # ):
+    #     # TODO: Reference flipper_dent, flipper_tend, maybe as instance variables or import
+
+    #     bid = flipper["bids"][bid_id]
+
+    #     if stance and (
+    #         "gas_price" not in stance
+    #         or spotter["ilks"]["gas"]["val"] <= stance["gas_price"]
+    #     ):
+    #         price = stance["price"]
+
+    #         if bid["bid"] == bid["tab"]:
+    #             # Dent phase
+    #             our_lot = Wad(bid["bid"] / Rad(price))
+    #             if (
+    #                 our_lot * flipper["beg"] <= bid["lot"]
+    #                 and our_lot < bid["lot"]
+    #                 and vat["dai"][user_id] >= bid["bid"]
+    #             ):
+    #                 flipper_dent(
+    #                     flipper, vat, bid_id, user_id, our_lot, bid["bid"], now
+    #                 )
+    #                 stats["num_bids"] += 1
+
+    #         else:
+    #             # Tend phase
+    #             our_bid = Rad.min(Rad(bid["lot"]) * price, bid["tab"])
+    #             if (
+    #                 (our_bid >= bid["bid"] * flipper["beg"] or our_bid == bid["tab"])
+    #                 and our_bid > bid["bid"]
+    #                 and vat["dai"][user_id]
+    #                 >= (our_bid if user_id != bid["guy"] else our_bid - bid["bid"])
+    #             ):
+    #                 flipper_tend(
+    #                     flipper, vat, bid_id, user_id, bid["lot"], our_bid, now
+    #                 )
+    #                 stats["num_bids"] += 1
