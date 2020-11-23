ae2ffd0c0cb0a84a7e3a91da67b2e4a3ee546561
Sehyun Chung <sehyun@berkeley.edu>
2020-11-22 20:53:58 -0500

    refactor dent function

--- dai_cadcad/flip.py
+++ dai_cadcad/flip.py
@@ -13,6 +13,7 @@ class Flipper:
     tau = None
     kicks = None
     bids = {}
+    ilk = None
 
     def __init__(self, vat, beg, ttl, tau, kicks, bids, ilk):
         """"""
@@ -26,6 +27,7 @@ class Flipper:
         self.tau = tau
         self.kicks = kicks
         self.bids = bids
+        self.ilk = ilk
 
     def kick(flipper, vat, usr, gal, tab, lot, bid, end):
         """Kicks off a new Flip auction."""
@@ -46,10 +48,10 @@ class Flipper:
 
         vat_flux(vat, flipper["ilk"], "cat", "flipper_eth", lot)
 
-    def tend(flipper, vat, bid_id, usr, lot, bid, now):
+    def tend(self, vat, bid_id, usr, lot, bid, now):
         """Places a tend bid on a Flipper auction."""
 
-        curr_bid = flipper["bids"][bid_id]
+        curr_bid = self.bids[bid_id]
 
         assert (
             curr_bid["tic"] > now or curr_bid["tic"] == 0
@@ -60,7 +62,7 @@ class Flipper:
         assert bid <= curr_bid["tab"], "Flipper/higher-than-tab"
         assert bid > curr_bid["bid"], "Flipper/bid-not-higher"
         assert (
-            bid >= curr_bid["bid"] * flipper["beg"] or bid == curr_bid["tab"]
+            bid >= curr_bid["bid"] * self.beg or bid == curr_bid["tab"]
         ), "Flipper/insufficient-increase"
 
         if usr != curr_bid["guy"]:
@@ -69,12 +71,12 @@ class Flipper:
         vat_move(vat, usr, curr_bid["gal"], bid - curr_bid["bid"])
 
         curr_bid["bid"] = bid
-        curr_bid["tic"] = now + flipper["ttl"]
+        curr_bid["tic"] = now + self.ttl
 
-    def dent(flipper, vat, bid_id, usr, lot, bid, now):
+    def dent(self, vat, bid_id, usr, lot, bid, now):
         """Places a dent bid on a Flipper auction."""
 
-        curr_bid = flipper["bids"][bid_id]
+        curr_bid = self.bids[bid_id]
 
         assert (
             curr_bid["tic"] > now or curr_bid["tic"] == 0
@@ -84,17 +86,15 @@ class Flipper:
         assert bid == curr_bid["bid"], "Flipper/not-matching-bid"
         assert bid == curr_bid["tab"], "Flipper/tend-not-finished"
         assert lot < curr_bid["lot"], "Flipper/lot-not-lower"
-        assert lot * flipper["beg"] <= curr_bid["lot"], "Flipper/insufficient-decrease"
+        assert lot * self.beg <= curr_bid["lot"], "Flipper/insufficient-decrease"
 
         if usr != curr_bid["guy"]:
             vat_move(vat, usr, curr_bid["guy"], curr_bid["bid"])
             curr_bid["guy"] = usr
-        vat_flux(
-            vat, flipper["ilk"], "flipper_eth", curr_bid["usr"], curr_bid["lot"] - lot
-        )
+        vat_flux(vat, self.ilk, "flipper_eth", curr_bid["usr"], curr_bid["lot"] - lot)
 
         curr_bid["lot"] = lot
-        curr_bid["tic"] = now + flipper["ttl"]
+        curr_bid["tic"] = now + self.ttl
 
     def deal(flipper, vat, cat, bid_id, now):
         """Deals out a Flipper auction."""
