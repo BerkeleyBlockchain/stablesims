9a2e34b00280aa3ca8ed0ec279acf441bc58f366
Sehyun Chung <sehyun@berkeley.edu>
2020-12-06 02:56:12 -0500

    replace snowpack with webpack

---
+++ demo-frontend/src/pages/Experiments.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Button } from '@chakra-ui/react';
+
+export default function Experiments() {
+  return (
+    <>
+      <Button>Black Thursday</Button>
+    </>
+  );
+}
